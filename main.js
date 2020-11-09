973f16262c6bdfc6c04623c6996688e74078b153
Spencer Heffley <spheffley@yahoo.com>
2020-11-09 14:51:19 -0500

    Added array for grid info

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+//array for each grid item
+const ticTacToeGrid = [
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'},
+  {empty: true,
+    value: 'none'}
+];
