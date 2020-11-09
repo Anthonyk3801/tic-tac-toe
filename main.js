c03064fe43e81d73cb2f2558b1b4c7918d775f25
Spencer Heffley <spheffley@yahoo.com>
2020-11-09 15:18:13 -0500

    Added the first of the main function, an X shows up in the first cell only after clicking the first cell

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ const ticTacToeGrid = [
   {empty: true,
     value: 'none'}
 ];
+
+//pull all elements from html
+const grid = document.querySelector('.grid');
+const gridCells = Array.from(document.querySelectorAll('div div'));
+
+//function to play the game, add x or o if cell is empty
+const addXO = function() {
+  //add event listener for the whole grid, but check which box was clicked
+  grid.addEventListener('click', place = () => {
+    let clickedCell = ticTacToeGrid[Number(event.target.id)];
+    console.log(clickedCell);
+
+    if (event.target.id == "0") {
+      const x = gridCells[0].querySelector('span');
+      x.style.display = 'block';
+      ticTacToeGrid[0].empty = false;
+    }
+  });
+}
+addXO();
