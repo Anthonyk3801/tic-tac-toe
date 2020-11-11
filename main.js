806cbb2e5e2af9e440808b2aca16bf8ff71722c6
Spencer Heffley <spheffley@yahoo.com>
2020-11-11 14:57:13 -0500

    added placing of Os, and alternates between x and o

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ const ticTacToeGrid = [
 const grid = document.querySelector('.TTT-grid');
 const gridCells = Array.from(document.querySelectorAll('.spot'));
 
-//function to play the game, add x or o if cell is empty
+//function to play the game, add x if cell is empty
 const addX = function() {
   //add event listener for the whole grid, but check which box was clicked
-  grid.addEventListener('click', place = () => {
+  grid.addEventListener('click', placeX = () => {
     let clickedCell = ticTacToeGrid[Number(event.target.id)];
     console.log(clickedCell);
     if (clickedCell.empty == true) {
@@ -85,6 +85,75 @@ const addX = function() {
         ticTacToeGrid[8].empty = false;
         ticTacToeGrid[8].value = 'x';
       }
+      grid.removeEventListener('click', placeX);
+      addO();
+    }
+  });
+}
+
+//function to play the game, add o if cell is empty
+const addO = function() {
+  //add event listener for the whole grid, but check which box was clicked
+  grid.addEventListener('click', placeO = () => {
+    let clickedCell = ticTacToeGrid[Number(event.target.id)];
+    console.log(clickedCell);
+    if (clickedCell.empty == true) {
+      if (event.target.id == "0") {
+        const o = gridCells[0].querySelectorAll('span')[0];
+        x.style.display = 'block';
+        ticTacToeGrid[0].empty = false;
+        ticTacToeGrid[0].value = 'o';
+      }
+      else if (event.target.id == "1") {
+        const o = gridCells[1].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[1].empty = false;
+        ticTacToeGrid[1].value = 'o';
+      }
+      else if (event.target.id == "2") {
+        const o = gridCells[2].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[2].empty = false;
+        ticTacToeGrid[2].value = 'o';
+      }
+      else if (event.target.id == "3") {
+        const o = gridCells[3].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[3].empty = false;
+        ticTacToeGrid[3].value = 'o';
+      }
+      else if (event.target.id == "4") {
+        const o = gridCells[4].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[4].empty = false;
+        ticTacToeGrid[4].value = 'o';
+      }
+      else if (event.target.id == "5") {
+        const o = gridCells[5].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[5].empty = false;
+        ticTacToeGrid[5].value = 'o';
+      }
+      else if (event.target.id == "6") {
+        const o = gridCells[6].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[6].empty = false;
+        ticTacToeGrid[6].value = 'o';
+      }
+      else if (event.target.id == "7") {
+        const o = gridCells[7].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[7].empty = false;
+        ticTacToeGrid[7].value = 'o';
+      }
+      else if (event.target.id == "8") {
+        const o = gridCells[8].querySelectorAll('span')[1];
+        o.style.display = 'block';
+        ticTacToeGrid[8].empty = false;
+        ticTacToeGrid[8].value = 'o';
+      }
+      grid.removeEventListener('click', placeO);
+      addX();
     }
   });
 }
